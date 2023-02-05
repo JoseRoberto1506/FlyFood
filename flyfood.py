c011fe91369e40f0336f3c77d9a9265af4716b60
José Roberto Araújo <84887543+JoseRoberto1506@users.noreply.github.com>
2023-02-04 21:27:10 -0300

    Primeiros arquivos

---
+++ flyfood.py
@@ -0,0 +1,108 @@
+from time import time
+
+
+def main():
+    linhas, colunas, matriz = ler_matriz()
+
+    pontos_de_entrega, coordenadas = pegar_pontos_de_entrega(linhas, colunas, matriz)
+
+    inicio = time()
+
+    rotas = permutar(pontos_de_entrega)
+
+    calcular_custo_das_rotas(rotas, coordenadas)
+
+    fim = time()
+    print(f"Tempo total: {fim - inicio} segundos")
+
+
+def ler_matriz():
+    with open("matriz.txt", "r") as arquivo:
+        # Pegar a quantidade de linhas e colunas da matriz
+        quantidade_linhas, quantidade_colunas = arquivo.readline().split()
+        
+        # Ler cada linha da matriz
+        matriz_lida = []
+        for linha in arquivo.readlines():
+            matriz_lida.append(list(linha))
+        
+        # Remover as quebras de linha ('\n') de cada linha 
+        for elemento in matriz_lida:
+            if elemento[-1] == '\n':
+                del elemento[-1]
+        
+    return int(quantidade_linhas), int(quantidade_colunas), matriz_lida
+
+
+def pegar_pontos_de_entrega(linha, coluna, Matriz):
+    pontos = []
+    pontos_e_coordenadas = {}
+
+    for i in range(linha):
+        for j in range(coluna):
+            if Matriz[i][j].isalpha():
+                pontos.append(Matriz[i][j])
+                pontos_e_coordenadas[Matriz[i][j]] = (i, j)
+    
+    pontos.remove('R')
+    pontos.sort()
+
+    return pontos, pontos_e_coordenadas
+
+
+'''
+def xyz(lista):
+    for cada ponto não visitado
+        for combinação em (xyz() - lista[0])
+'''
+
+
+def permutar(pontos):
+    if len(pontos) == 1:
+        return [pontos]
+    
+    # Lista para armazenar as combinações de rotas
+    combinações = []
+
+    # Travando um ponto pelo seu index
+    for ponto_travado in range(len(pontos)):
+        pontos_nao_visitados = pontos[0:ponto_travado] + pontos[ponto_travado + 1:]
+        # Concatenando cada combinação de rota dos pontos não visitados com o ponto travado
+        for ponto in permutar(pontos_nao_visitados):
+            combinações.append([pontos[ponto_travado]] + ponto)
+
+    return combinações
+
+
+def calcular_custo_das_rotas(caminhos, coords):
+    custo_menor_caminho = 9999
+    menor_caminho = []
+
+    for caminho in caminhos:
+        custo_caminho_atual = 0
+
+        # Colocando o ponto de origem e retorno no caminho
+        caminho.insert(0, 'R')
+        caminho.append('R')
+
+        # Calculando a distância entre os pontos adjacentes de cada caminho
+        for i in range(len(caminho) - 1):
+            coord_ponto_1 = coords[caminho[i]]
+            coord_ponto_2 = coords[caminho[i + 1]]
+            custo_caminho_atual += calcular_distancia_entre_pontos(coord_ponto_1, coord_ponto_2)
+
+        if custo_caminho_atual < custo_menor_caminho:
+            custo_menor_caminho = custo_caminho_atual
+            menor_caminho = caminho
+
+    print(f"Rota de menor distância: '{' '.join(menor_caminho)}'")
+    print(f"Distância da menor rota: {custo_menor_caminho} dronômetros")
+
+
+def calcular_distancia_entre_pontos(ponto_1, ponto_2):
+    distancia = abs(ponto_1[0] - ponto_2[0]) + abs(ponto_1[1] - ponto_2[1])
+    
+    return distancia
+
+
+main()
